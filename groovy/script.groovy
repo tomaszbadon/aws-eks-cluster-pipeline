386237957c415a63baf3b2910e841cfc998cb45d
Tomasz Badoń <tomasz.badon@hotmail.com>
2024-12-23 12:55:08 +0100

    ii

--- groovy/script.groovy
+++ groovy/script.groovy
@@ -1,20 +1,24 @@
 def s3BucketExist() {
     def status = sh(script: "aws s3api head-bucket --bucket $S3_BUCKET_NAME", returnStatus: true)
-    if (status != 0) {
-        status = sh(script:"aws s3api create-bucket --bucket $S3_BUCKET_NAME --region $AWS_DEFAULT_REGION --create-bucket-configuration LocationConstraint=$AWS_DEFAULT_REGION", returnStatus: true)
-        echo "The S3 Bucket: ${S3_BUCKET_NAME} created with status: ${status}"
-        env.S3_BUCKET_EXISTS = 'false'
-    } else {
-        env.S3_BUCKET_EXISTS = 'true'
-    }
+    env.S3_BUCKET_EXISTS = status == 0 ? 'true' : 'false'
+}
+
+def createS3Bucket() {
+    def command = """aws s3api create-bucket \
+            --bucket $S3_BUCKET_NAME \
+            --region $AWS_DEFAULT_REGION \
+            --create-bucket-configuration \
+            LocationConstraint=$AWS_DEFAULT_REGION"""
+    def status = sh(script: command, returnStatus: true)
+    echo "The S3 Bucket: ${S3_BUCKET_NAME} created with status: ${status}"
 }
 
 def testApp() {
     echo 'testing the application...'
-} 
+}
 
 def deployApp() {
     echo 'deplying the application...'
-} 
+}
 
 return this
