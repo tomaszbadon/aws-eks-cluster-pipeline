319accee5ec3960743e74aac76000fcc61179c25
Tomasz Badoń <tomasz.badon@hotmail.com>
2024-12-23 12:15:52 +0100

    oo

--- groovy/script.groovy
+++ groovy/script.groovy
@@ -1,3 +1,13 @@
-defbucketExists() {
-    echo 'Hello World'
-}
+def buildApp() {
+    echo 'building the application...'
+} 
+
+def testApp() {
+    echo 'testing the application...'
+} 
+
+def deployApp() {
+    echo 'deplying the application...'
+} 
+
+return this
